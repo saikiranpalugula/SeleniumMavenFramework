fd71f5a8377ca5a84021dd6a500147f46d724963
Saikiran <saii2882@gmail.com>
2026-03-19 21:52:16 +0530

    added test data

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -2,25 +2,43 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import utils.Log;
 
 public class LoginPage {
 
     private WebDriver driver;
 
     private By usernameTxtbox = By.id("Email");
-    private By passwdTxtbox = By.cssSelector("#Password");
-    private By loginBtn = By.xpath("//*[text()='Log in']");
+
+    //private By passwdTxtbox = By.cssSelector("#Password");
+    @FindBy(css="#Password")
+    WebElement passwdTxtbox;
+
+    @FindBy(xpath = "//*[text()='Log in']")
+    WebElement loginBtn;
+    //private By loginBtn = By.xpath("//*[text()='Log in']");
 
     public LoginPage(WebDriver driver){
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void login(String userName, String passWord){
         driver.findElement(usernameTxtbox).clear();
         driver.findElement(usernameTxtbox).sendKeys(userName);
-        driver.findElement(passwdTxtbox).clear();
+
+        passwdTxtbox.clear();
+        passwdTxtbox.sendKeys(passWord);
+        /*driver.findElement(passwdTxtbox).clear();
         driver.findElement(passwdTxtbox).sendKeys(passWord);
-        driver.findElement(loginBtn).click();
+        driver.findElement(loginBtn).click();*/
+
+        loginBtn.click();
+        Assert.assertEquals(driver.getTitle(),"Just a moment...");
     }
 
     public void enterUsername(String username){
@@ -29,11 +47,16 @@ public class LoginPage {
     }
 
     public void enterPassword(String password){
-        driver.findElement(passwdTxtbox).clear();
-        driver.findElement(passwdTxtbox).sendKeys(password);
+       /* driver.findElement(passwdTxtbox).clear();
+        driver.findElement(passwdTxtbox).sendKeys(password);*/
+
+        passwdTxtbox.clear();
+        passwdTxtbox.sendKeys(password);
     }
 
     public void clickLogin(){
-        driver.findElement(loginBtn).click();
+        //driver.findElement(loginBtn).click();
+        loginBtn.click();
+        Log.info("Clicked on Login button");
     }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,20 +1,59 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import utils.ExcelUtils;
+import utils.ExtentReportManager;
+import utils.Log;
+
+import java.io.IOException;
 
 public class LoginTest extends BaseTest {
 
-    @Test
-    public void testValidLogin(){
+    @DataProvider(name="LoginData")
+    public Object[][] getLoginData() throws IOException {
+        String filePath=System.getProperty("user.dir")+"/src/test/resources/testdata/TestData.xlsx";
+        ExcelUtils.loadExcel(filePath,"Sheet1");
+        int rowCount = ExcelUtils.getRowCount();
+        Object[][] data = new Object[rowCount-1][2];
+        for(int i=1;i<rowCount;i++){
+            data[i-1][0]=ExcelUtils.getCellData(i,0);
+            data[i-1][1]=ExcelUtils.getCellData(i,1);
+        }
+        ExcelUtils.closeExcel();
+        return data;
+    }
+
+    @Test(dataProvider = "LoginData")
+    public void testValidLogin( String username, String password){
         LoginPage loginpg = new LoginPage(driver);
         /*loginpg.enterUsername("admin@yourstore.com");
         loginpg.enterPassword("admin");
         loginpg.clickLogin();
         System.out.println(driver.getTitle());*/
+        Log.info("Starting Login Test");
+        test = ExtentReportManager.createTest("Login Test with "+ username);
+        test.info("Adding credentials");
+        loginpg.login(username, password);
+        test.info("Login completed");
+        test.pass("Login Successfully");
+    }
 
-        loginpg.login("admin@yourstore.com", "admin");
+    @Test(enabled = false)
+    public void testInValidLogin(){
+        LoginPage loginpg = new LoginPage(driver);
+        /*loginpg.enterUsername("admin@yourstore.com");
+        loginpg.enterPassword("admin");
+        loginpg.clickLogin();
+        System.out.println(driver.getTitle());*/
+        Log.info("Starting Login Test");
+        test = ExtentReportManager.createTest("Login Test");
+        test.info("Adding credentials");
+        loginpg.login("admin@yourstore.com", "admin...");
+        test.info("Login completed");
+        test.pass("Login Successfully");
     }
 
 
